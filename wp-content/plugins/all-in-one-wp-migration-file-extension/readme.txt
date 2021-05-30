ffeabe05e1e6517f1e26749b016a33888597407c
Riya <riyaasenn1995@gmail.com>
2021-05-30 14:05:53 +0530

    delete

--- wp-content/plugins/all-in-one-wp-migration-file-extension/readme.txt
+++
@@ -1,2 +0,0 @@
-Install instructions: https://help.servmask.com/knowledgebase/install-instructions-for-file-extension/
-User guide: https://help.servmask.com/knowledgebase/file-extension-user-guide/
--- wp-content/plugins/elementor/commitlint.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	extends: [ '@commitlint/config-conventional' ],
-	rules: {
-		'subject-case': [ 2, 'always', 'sentence-case' ],
-		'type-case': [ 2, 'always', 'sentence-case' ],
-		'type-enum': [ 2, 'always', [ 'New', 'Tweak', 'Fix', 'Experiment', 'Deprecate', 'Deprecated', 'Revert' ] ],
-	},
-};
